8c1a92f56428869acc99a2e0768d9a9819f7764d
Maricoellofreitas <mcoellofreitas@gmail.com>
2023-08-13 20:08:12 -0300

    funcionando

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    var div = document.getElementById("eldiv"); 
+    div.addEventListener("click",() => {
+        alert("Hola soy el div");
+    });
+});
