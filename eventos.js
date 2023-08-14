58bfcb5dc57ecca66e615027d31a36253e88e323
Maricoellofreitas <mcoellofreitas@gmail.com>
2023-08-13 21:15:56 -0300

    solucion

--- eventos.js
+++ eventos.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", () =>{
     var div = document.getElementById("eldiv"); 
     div.addEventListener("click",() => {
-        alert("Hola soy el div");
+         alert("Hola soy el div");
+    });
+
+    var boton =document.getElementById("boton");
+    boton.addEventListener("click",(event) => {
+        event.stopPropagation();
     });
 });
